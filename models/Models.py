feb8d047e72d06400a7b627664c717419bb86331
shawnwan47 <shawnwan47@gmail.com>
2018-05-02 21:32:48 +0800

    guess without the modules.py in models/ is better...

--- models/Models.py
+++ models/Models.py
@@ -5,8 +5,20 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 import models
-from models.modules import DayTimeEmbedding
 from models import Seq2Seq, Tensor2Tensor, RNN
+
+
+class DayTimeEmbedding(nn.Module):
+    def __init__(self, num_time, time_size, day_size, pdrop=0):
+        super().__init__()
+        self.embedding_day = nn.Embedding(7, day_size)
+        self.embedding_time = nn.Embedding(num_time, time_size)
+        self.dropout = nn.Dropout(pdrop)
+
+    def forward(self, data_cat):
+        embedded_day = self.embedding_day(data_cat[:, :, 0])
+        embedded_time = self.embedding_time(data_cat[:, :, 1])
+        return self.dropout(torch.cat((embedded_time, embedded_day), dim=-1))
 
 
 def build_model(args):
