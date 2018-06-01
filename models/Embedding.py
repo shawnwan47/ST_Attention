6fffe1e88d93bb45d743fbe12f1fa63702f30d06
shawnwan47 <shawnwan47@gmail.com>
2018-06-01 19:55:59 +0800

    embedding with input data, time, weekday.

--- models/Embedding.py
+++ models/Embedding.py
@@ -17,8 +17,8 @@ class TempEmbedding(nn.Module):
         if use_weekday:
             self.embedding_weekday = nn.Embedding(num_weekday, weekday_dim)
 
-    def forward(self, time, weekday):
-        output = []
+    def forward(self, data, time, weekday):
+        output = [data]
         if self.use_time:
             output.append(self.dropout(self.embedding_time(time)))
         if self.use_weekday:
@@ -31,13 +31,13 @@ class STEmbedding(nn.Module):
                  num_node, node_dim,
                  num_time, time_dim,
                  num_weekday, weekday_dim,
-                 dropout):
+                 input_size, hidden_size, dropout):
         super().__init__()
         self.use_node = use_node
         self.use_time = use_time
         self.use_weekday = use_weekday
-        self.num_node = num_node
         self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(input_size, hidden_size)
         if use_node:
             self.embedding_node = nn.Embedding(num_node, node_dim)
         if use_time:
@@ -45,16 +45,15 @@ class STEmbedding(nn.Module):
         if use_weekday:
             self.embedding_weekday = nn.Embedding(num_weekday, weekday_dim)
 
-    def forward(self, time, weekday):
-        batch, seq = time.size()
-        shape = (batch, seq, self.num_node, -1)
-        output = []
-
+    def forward(self, data, time, weekday):
+        batch, seq, num_node, _ = data.size()
+        shape = (batch, seq, num_node, -1)
+        output = [data]
         if self.use_node:
-            node = time.new_tensor(torch.arange(self.num_node))
+            node = time.new_tensor(torch.arange(num_node))
             output.append(self.embedding_node(node).expand(shape))
         if self.use_time:
             output.append(self.embedding_time(time).unsqueeze(-2).expand(shape))
         if self.use_weekday:
             output.append(self.embedding_weekday(weekday).unsqueeze(-2).expand(shape))
-        return self.dropout(torch.cat(output, -1))
+        return self.linear(self.dropout(torch.cat(output, -1)))
