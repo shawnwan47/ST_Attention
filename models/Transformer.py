658a0fd3fcb8a684123785ef4a0256f8e4f2f5db
shawnwan47 <shawnwan47@gmail.com>
2018-06-01 11:50:06 +0800

    123

--- models/Transformer.py
+++ models/Transformer.py
@@ -47,6 +47,7 @@ class PositionwiseFeedForward(nn.Module):
         self.w_1 = nn.Linear(size, hidden_size)
         self.w_2 = nn.Linear(hidden_size, size)
         self.layer_norm = nn.LayerNorm(size)
+        # Save a little memory, by doing inplace.
         self.dropout_1 = nn.Dropout(dropout, inplace=True)
         self.relu = nn.ReLU(inplace=True)
         self.dropout_2 = nn.Dropout(dropout)
