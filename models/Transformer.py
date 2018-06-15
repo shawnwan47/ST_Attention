a783f5f6525209d1e88e1d367dbc11508c5a2645
Shawn Wan <shawnwan47@gmail.com>
2018-06-15 09:51:27 +0800

    Transformer and STTransformer almost ready

--- models/Transformer.py
+++ models/Transformer.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def mask_temporal(self, size=24):
+def gen_mask_temporal(self, size=24):
     ''' Get an attention mask to avoid using the future info.'''
     attn_shape = (1, size, size)
     subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
     subsequent_mask = torch.from_numpy(subsequent_mask)
     return subsequent_mask
+
 
 class PositionwiseFeedForward(nn.Module):
     def __init__(self, size, dropout=0.1):
@@ -32,7 +33,7 @@ class Transformer(nn.Module):
             TransformerLayer(size, head_count, dropout)
             for i in range(num_layers)
         ])
-        self.register_buffer('mask', mask_temporal())
+        self.register_buffer('mask', gen_mask_temporal())
 
     def forward(self, input, bank=None):
         len_input = input.size(-2)
@@ -40,30 +41,30 @@ class Transformer(nn.Module):
             mask = self.mask[-len_input:, -len_input:]
         else:
             mask = self.mask[-len_input:, -(len_input + bank.size(-2)):]
-        output, attn = [input], []
+        query, context, attn = input, [], []
         for i, layer in enumerate(self.layers):
             if bank is None:
-                context = output[-1]
+                context.append(query)
             else:
-                context = torch.cat((bank[:, i], output[-1]), -2)
-            output_i, attn_i = layer(output[-1], context, mask)
-            output.append(output_i)
+                context.append(torch.cat((bank[:, i], query), -2))
+            query, attn_i = layer(query, context[-1], mask)
             attn.append(attn_i)
-        return output[-1], torch.stack(output, 1), torch.stack(attn, 1)
+        context = torch.stack(context, 1)
+        attn = torch.stack(attn, 1)
+        return query, context, attn
 
 
 class TransformerLayer(nn.Module):
     def __init__(self, size, head_count, dropout):
         super().__init__()
-        self.attention = Attention.MultiAttention(size, size, head_count, dropout)
+        self.attention = Attention.MultiAttention(size, head_count, dropout)
         self.layer_norm = nn.LayerNorm(size)
         self.feed_forward = PositionwiseFeedForward(size, dropout)
         self.dropout = nn.Dropout(dropout, inplace=True)
 
     def forward(self, query, context, mask):
         context, attn = self.attention(context, context, query, mask)
-        output = self.dropout(context) + query
-        output = self.layer_norm(output)
+        output = self.layer_norm(self.dropout(context) + query)
         output = self.feed_forward(output)
         return output, attn
 
@@ -75,7 +76,7 @@ class STTransformer(nn.Module):
             STTransformerLayer(size, head_count, dropout)
             for i in range(num_layers)
         ])
-        self.register_buffer('mask', mask_temporal())
+        self.register_buffer('mask', gen_mask_temporal())
 
     def forward(self, input, bank=None):
         len_input = input.size(-3)
@@ -83,27 +84,35 @@ class STTransformer(nn.Module):
             mask = self.mask[-len_input:, -len_input:]
         else:
             mask = self.mask[-len_input:, -(len_input + bank.size(-3)):]
-        output, attn_temporal, attn_spatial = [input], [], []
+        query, context, attn_temporal, attn_spatial = input, [], [], []
         for i, layer in enumerate(self.layers):
             if bank is None:
-                context = output[-1]
+                context.append(query)
             else:
-                context = torch.cat((bank[:, i], output[-1]), -3)
-            output_i, attn_t_i, attn_s_i = layer(output[-1], context, mask)
-            output.append(output_i)
-            attn_temporal.append(attn_t_i)
-            attn_spatial.append(attn_s_i)
+                context.append(torch.cat((bank[:, i], query), -3))
+            query, attn_t, attn_s = layer(query, context[-1], mask)
+            attn_temporal.append(attn_t)
+            attn_spatial.append(attn_s)
+        context = torch.stack(context, 1)
         attn_temporal = torch.stack(attn_temporal, 1)
         attn_spatial = torch.stack(attn_spatial, 1)
-        return output[-1], torch.stack(output, 1), attn_temporal, attn_spatial
+        return query, context, attn_temporal, attn_spatial
 
 
 class STTransformerLayer(nn.Module):
     def __init__(self, size, head_count, dropout):
-        self.attn_temporal = Attention.MultiAttention(size, head_count, dropout)
-        self.attn_spatial = Attention.MultiAttention(size, head_count, dropout)
-        self.layer_norm_temporal = nn.LayerNorm(size)
-        self.layer_norm_spatial = nn.LayerNorm(size)
+        self.attention_s = Attention.MultiAttention(size, head_count, dropout)
+        self.attention_t = Attention.MultiAttention(size, head_count, dropout)
         self.dropout = nn.Dropout(dropout, inplace=True)
+        self.layer_norm = nn.LayerNorm(size)
+        self.feed_forward = PositionwiseFeedForward(size, dropout)
 
-    def forward(self, query, context):
+    def forward(self, query, context, mask=None):
+        context_s, attn_s = self.attention_s(query, context)
+        query_t, context_t = query.transpose(-2, -3), context.transpose(-2, -3)
+        context_t, attn_t = self.attention_t(query_t, context_t)
+        context_t = context_t.transpose(-2, -3)
+        output = query + self.dropout(context_s) + self.dropout(context_t)
+        output = self.layer_norm(output)
+        output = self.feed_forward(output)
+        return output, attn_s, attn_t
