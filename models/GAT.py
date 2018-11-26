ee5e783e9d16090b0f70bc820b08fe49cd27af59
Shawn Wan <shawnwan47@gmail.com>
2018-11-26 09:15:50 +0800

    hey man, let's get started!

--- models/GAT.py
+++ models/GAT.py
@@ -5,7 +5,7 @@ from models import Attention
 
 
 class GraphAttention(nn.Module):
-    def __init__(self, input_size, output_size, head_count, dropout, mask=None):
+    def __init__(self, input_size, output_size, head_count, dropout=0.1, mask=None):
         super().__init__()
         self.attention = Attention.MultiAttention(
             size=input_size,
@@ -23,6 +23,27 @@ class GraphAttention(nn.Module):
         context, attn = self.attention(input, input, input, self.mask)
         output = self.linear_query(input) + context
         return output, attn
+
+
+class GraphAttentionHighway(nn.Module):
+    def __init__(self, input_size, output_size, head_count, dropout=0.1, mask=None):
+        super().__init__()
+        self.attention = Attention.MultiAttention(
+            size=input_size,
+            head_count=head_count,
+            dropout=dropout,
+            output_size=output_size
+        )
+        self.linear_query = nn.Linear(input_size, output_size, bias=False)
+        self.input_gate = nn.Linear(input_size, 1)
+        self.output_gate = nn.Linear(output_size, 1)
+        self.highway = nn.Sigmoid()
+        self.register_buffer('mask', mask)
+
+    def forward(self, input):
+        context, attn = self.attention(input, input, input, self.mask)
+        gate = self.highway(self.input_gate(input), self.output_gate(context))
+        output =
 
 
 class GraphRelativeAttention(GraphAttention):
