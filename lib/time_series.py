35f8cc26879f520a0eac564996eb316257d376c8
Shawn Wan <shawnwan47@gmail.com>
2019-01-08 22:51:44 +0800

    let us run!

--- lib/time_series.py
+++ lib/time_series.py
@@ -6,25 +6,21 @@ from torch.utils.data import Dataset
 class TimeSeries(Dataset):
     def __init__(self, df, mean, std, history, horizon):
         super().__init__()
-        self.data, self.time, self.weekday, self.target = self._df_to_io(df, mean, std)
-        self.history = history
-        self.horizon = horizon
-        self.days = self.data.size(0)
-        self.daily_samples = self.data.size(1) - horizon
+        samples = self.gen_samples(df, mean, std, history, horizon)
+        self.data, self.time, self.weekday, self.target = samples
 
     def __len__(self):
-        return self.days * self.daily_samples
+        return self.data.size(0)
 
     def __getitem__(self, index):
-        d, t = divmod(index, self.daily_samples)
-        data = self.data[d, t:t+self.history]
-        time = self.time[d, t:t+self.history]
-        weekday = self.weekday[d]
-        target = self.target[d, t+self.history:t+self.history+self.horizon]
+        data = self.data[index]
+        time = self.time[index]
+        weekday = self.weekday[index]
+        target = self.target[index]
         return data, time, weekday, target
 
     @staticmethod
-    def _df_to_io(df, mean, std):
+    def gen_samples(df, mean, std, history, horizon):
         # extract data
         data = (df.values - mean) / (std + 1e-8)
         data[np.isnan(data)] = 0
@@ -38,8 +34,20 @@ class TimeSeries(Dataset):
 
         data = data.reshape(num_days, -1, num_nodes)
         time = time.reshape(num_days, -1)
-        weekday = weekday.reshape(num_days, -1)[:, 0]
+        weekday = weekday.reshape(num_days, -1)
         target = target.reshape(num_days, -1, num_nodes)
+
+        daily = data.shape[1] - history - horizon
+        data = np.stack([data[:, i:i+history] for i in range(daily)], 1)
+        time = np.stack([time[:, i:i+history] for i in range(daily)], 1)
+        weekday = np.stack([weekday[:, i] for i in range(daily)], 1)
+        target = [target[:, i+history:i+history+horizon] for i in range(daily)]
+        target = np.stack(target, 1)
+
+        data = data.reshape(-1, history, num_nodes)
+        time = time.reshape(-1, history)
+        weekday = weekday.reshape(-1)
+        target = target.reshape(-1, horizon, num_nodes)
 
         data = torch.FloatTensor(data)
         time = torch.LongTensor(time)
